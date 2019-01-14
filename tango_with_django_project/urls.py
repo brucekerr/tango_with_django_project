f70b897a161a378d2dc9bf3e02d1a7ea9c159040
Bruce Kerr <46094669+brucekerr@users.noreply.github.com>
2019-01-14 23:23:43 +0000

    chapter 4 complete and unnecessary code from project/urls

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -20,13 +20,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^$', views.index, name="index"),
-    url(r'^index/', include('rango.urls')),
-
     url(r'^rango/', include('rango.urls')),
-
-    url(r'^about/', views.about, name="about"),
-    url(r'^about/', include('rango.urls')),
-
     url(r'^admin/', admin.site.urls),
         ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+#url(r'^about/', views.about, name="about"),
+#url(r'^about/', include('rango.urls')),
+#url(r'^$', views.index, name="index"),
+#url(r'^index/', include('rango.urls')),
