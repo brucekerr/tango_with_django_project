c25626dcb1b0b2318a104c56f5a83b3e4d955801
Bruce Kerr <46094669+brucekerr@users.noreply.github.com>
2019-01-28 15:31:04 +0000

    chapter 9 upto exercises

--- rango/urls.py
+++ rango/urls.py
@@ -3,7 +3,6 @@ from rango import views
 from django.conf import settings
 from django.conf.urls.static import static
 
-
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^about/$', views.about, name='about'),
@@ -16,4 +15,20 @@ urlpatterns = [
     url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$',
         views.add_page,
         name='add_page'),
-    ]
+
+    url(r'^register/$',
+        views.register,
+        name='register'),
+
+    url(r'^login/$',
+        views.user_login,
+        name='login'),
+
+    url(r'^restricted/',
+        views.restricted,
+        name='restricted'),
+
+    url(r'^logout/$',
+        views.user_logout,
+        name='logout'),
+]
